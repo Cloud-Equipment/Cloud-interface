8f2e27797aa53ddccf94ec9227d4a16b35e3d2aa
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-11-29 14:10:32 +0100

    Setup

---
+++ src/components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div>
+            <div className="Sidebar">
+                <div className="logo"></div>
+                <div className="">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className="">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
