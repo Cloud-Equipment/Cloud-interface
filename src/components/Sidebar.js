917880b009ebc5faaee056d463bb5cc2f2dfc5dd
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-11-29 15:32:40 +0100

    Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,37 @@
 import React from 'react'
+import { SidebarIcon } from '../data/data'
+import Logo from '../Assets/IconAndLogo/Screenshot 2023-04-08 at 20.20 1.png'
 
 function Sidebar() {
     return (
         <div>
             <div className="Sidebar">
-                <div className="logo"></div>
-                <div className="">
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                <div className="logo">
+                    <img src={Logo} alt="" />
                 </div>
-                <div className="">
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                <div className="margin50"></div>
+                <div className="Icons">
+                    <div className="Top">
+                        <center>
+                            <ul>
+                                <li><img src={SidebarIcon.icon1} alt="" /></li>
+                                <li><img src={SidebarIcon.icon2} alt="" /></li>
+                                <li><img src={SidebarIcon.icon3} alt="" /></li>
+                                <li><img src={SidebarIcon.icon4} alt="" /></li>
+                                <li><img src={SidebarIcon.icon5} alt="" /></li>
+                            </ul>
+                        </center>
+                    </div>
+                    <div className="Last">
+                        <center>
+                        <ul>
+                            <li><img src={SidebarIcon.icon6} alt="" /></li>
+                            <li><img src={SidebarIcon.icon7} alt="" /></li>
+                            <li><img src={SidebarIcon.icon8} alt="" /></li>
+                        </ul>
+                        </center>
+                       
+                    </div>
                 </div>
             </div>
         </div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,23 @@
+import icon1 from '../Assets/IconAndLogo/Group 88.png'
+import icon2 from '../Assets/IconAndLogo/Group 91.png'
+import icon3 from '../Assets/IconAndLogo/Group 92.png'
+import icon4 from '../Assets/IconAndLogo/iconoir_wallet.png'
+import icon5 from '../Assets/IconAndLogo/Group 94.png'
+import icon6 from '../Assets/IconAndLogo/Group 75.png'
+import icon7 from '../Assets/IconAndLogo/info.png'
+import icon8 from '../Assets/IconAndLogo/tabler_logout.png'
+
+const SidebarIcon = {
+    "icon1": icon1,
+    "icon2": icon2,
+    "icon3": icon3,
+    "icon4": icon4,
+    "icon5": icon5,
+    "icon6": icon6,
+    "icon7": icon7,
+    "icon8": icon8,
+}
 
 
-const SidebarIcon = [
-    
-]
+
+export  {SidebarIcon}
