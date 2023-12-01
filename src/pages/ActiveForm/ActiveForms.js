5c9d9c5e41fbfe8dcbf86112884a04587a85ea2b
Batimehin Emmanuel Oluwatimilehin <ebatimehin@gmail.com>
2023-12-01 22:36:33 +0100

    Few changes

--- src/pages/ActiveForm/ActiveForms.js
+++ src/pages/ActiveForm/ActiveForms.js
@@ -3,15 +3,12 @@ import Sidebar from '../../components/Sidebar'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import Banner from './Banner'
-import { calculateHeight } from '../../utils/utils'
+
 import Partners from '../../components/Partners'
 import Form from './Form'
 // import Chat from '../../Assets/IconAndLogo/CHAT.png'
 
 function ActiveForms() {
-  const number = 95
-  let height = calculateHeight(number)
-
   return (
     <div>
       <div className="ActiveForm">
