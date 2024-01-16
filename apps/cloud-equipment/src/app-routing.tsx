0ddd45dfdb271db3fa19e3580780ec5627a8344f
HyOcee <eosemegbe@gmail.com>
2024-01-16 05:09:07 +0100

    update

--- apps/cloud-equipment/src/app-routing.tsx
+++ apps/cloud-equipment/src/app-routing.tsx
@@ -1,13 +1,20 @@
-import { useRoutes } from 'react-router-dom';
+import { Navigate, Outlet, useRoutes } from 'react-router-dom';
 import { Auth } from '@cloud-equipment/auth';
 import { Main } from './Pages/Main/Main';
+import { useSelector } from 'react-redux';
+import { IAppState } from './Store/store';
+
+const ProtectedRoute = () => {
+  const isAuthenticated = useSelector((state: IAppState) => state.auth.user);
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
 
 export const AppRouting = () => {
   return useRoutes([
     { path: '/auth/*', element: <Auth /> },
     {
-      path: "/*",
-      element: <Main />,
+      path: '/*',
+      element: <ProtectedRoute />,
     },
   ]);
 };
